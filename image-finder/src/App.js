916b2d0432b75e0638ac934f2e3a594750d2b821
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2022-01-10 20:42:53 +0200

    feature/Searchbar

--- image-finder/src/App.js
+++ image-finder/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Searchbar from './components/Searchbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+export default class App extends Component{
+
+    state = {
+        inquiry: '',
+    };
+
+    handleFormSubmit  = inquiry => {
+
+        this.setState({ inquiry });
+    
+    };
+
+    render() {
+        return (
+            <div>
+                <Searchbar onSubmit={this.handleFormSubmit} />
+                <ToastContainer autoClose={3000} />
+            </div>
+        );
+    }
+  
 }
 
-export default App;
+
+
--- image-finder/src/components/Searchbar/Searchbar.jsx
+++ image-finder/src/components/Searchbar/Searchbar.jsx
@@ -1,15 +1,78 @@
-<header class="searchbar">
-  <form class="form">
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+import { Component } from 'react';
+import { ImSearch } from 'react-icons/im';
+import { toast } from 'react-toastify';
+import s from './Searchbar.module.css';
+import PropTypes from "prop-types";
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>
+
+
+export default class Searchbar extends Component{
+    state = {
+        inquiry: '',
+    };
+
+    handleNameChange = (e) => {
+        this.setState({ inquiry: e.currentTarget.value});
+    };
+  
+
+     handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.inquiry.trim() === '') {
+      toast.error('Введите запрос ');
+      return;
+    }
+
+    this.props.onSubmit(this.state.inquiry);
+    this.setState({ inquiry: '' });
+    };
+    
+    render() {
+        return (
+            
+            <header className={s.searchbar}>
+                <form
+                    onSubmit={this.handleFormSubmit}
+                    className={s.form}>
+                    <button
+                        type="submit"
+                        className={s.button}>
+                        <ImSearch />
+                        
+                        <span className={s.button__label}>Search</span>
+                    </button>
+
+                    <input
+                        className={s.input}
+                        value={this.state.inquiry}
+                        onChange={this.handleNameChange}
+                        type="text"
+                        autocomplete="off"
+                        autofocus
+                        placeholder="Search images and photos"
+                    />
+                </form>
+            </header>
+        );
+    }
+
+}
+
+
+
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+
+
+
+
+
+
+
+
+
+
